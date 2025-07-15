2a4033c1e8ce8f28e41a30d9a9aea03af833164e
Nikita L. <nikitiy8@yandex.ru>
2025-07-16 00:59:20 +0300

    modify launch scripts

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t nikiatsik/timetable_manager:latest .
--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-docker build -t nikiatsik/timetable_manager:latest .
-
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
 
