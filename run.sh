6911fe1e5bac7086f9dac392b98914c56bea6b3d
Nikita L. <nikitiy8@yandex.ru>
2025-07-16 23:37:04 +0300

    modify docs, add hint on timetable page

--- run.sh
+++ run.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# сборка образа и применение конфигураций
+
+minikube start
+
+eval $(minikube -p minikube docker-env)
+
+minikube image build -t timetable_manager .
+
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
 
+# смотрим адрес для доступа к веб приложению
+
 kubectl get pods
 
-minikube service timetable-manager --url
+kubectl get services
+
+minikube ip
